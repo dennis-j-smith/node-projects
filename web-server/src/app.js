e302e355610d79275548ad7102bab5696e3dcf8a
Dennis Smith <dennis.j.smith95@gmail.com>
2020-02-26 21:11:42 -0500

    new

---
+++ web-server/src/app.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const express = require('express')
+
+console.log(__dirname);
+
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.send('<h1>Weather</h1>')
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+      Location: "Baltimore",
+      Temp: 47
+    })
+})
+
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000')
+})
