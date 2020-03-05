87cd7c23a7168f95ba5ea5e2bf74378179591e9a
Dennis Smith <dennis.j.smith95@gmail.com>
2020-03-04 21:13:48 -0500

    updated

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,42 @@
 const path = require('path')
 const express = require('express')
-
-console.log(__dirname);
-
+const hbs = require('hbs')
 
 const app = express()
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static dir to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
+    res.render('index', {
+      title: 'Weather App',
+      name: 'Dennis'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+      title: 'About Me',
+      name: 'Dennis'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+      title: 'Help Page',
+      helpmsg: 'My help message',
+      name: 'Dennis'
+    })
 })
 
 app.get('/weather', (req, res) => {
@@ -19,6 +46,21 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('notfound', {
+      title: 'Not Found',
+      msg: 'Help Article not found.',
+      name: 'Dennis'
+    })
+})
+
+app.get('*', (req,res) => {
+  res.render('notfound', {
+    title: 'Not Found',
+    msg: 'Page not found.',
+    name: 'Dennis'
+  })
+})
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000')
