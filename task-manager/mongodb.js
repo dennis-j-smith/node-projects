f7ba12d5c9645a47066d86861efaf802b63d21e0
Dennis Smith <dennis.j.smith95@gmail.com>
2020-08-13 21:04:06 -0400

    tasks

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,13 @@
 //CRUD
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+
+const {MongoClient, ObjectID} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const dataBaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
@@ -14,9 +17,32 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
         const db = client.db(dataBaseName)
 
+        db.collection('users').findOne({name: "Dennis", age: 49}, (error, user) => {
+            if (error) {
+                return console.log("Unable to fetch");
+            }
+            console.log(user);
+        })
+
+        db.collection('users').find({age: 47}).count((error, count) => {
+            console.log(count);
+        });
+
+        db.collection('users').find({age: 47}).toArray((error, users) => {
+            console.log(users);
+        });
+
+        db.collection('tasks').find({_id: new ObjectID("5e9b5fd2e728803f849021cd")}).toArray((error, tasks) => {
+            console.log(tasks);
+        });
+
+        db.collection('tasks').find({complete: false}).toArray((error, tasks) => {
+            console.log(tasks);
+        });
         // db.collection('users').insertOne({
-        //     name: 'Dennis',
-        //     age: 47
+        //     _id: id,
+        //     name: 'Maude',
+        //     age: 99
         // }, (error, result) => {
         //     if (error) {
         //         return console.log('Unable to insert user')
@@ -40,24 +66,26 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
         //     console.log(result.ops)
         // })
 
-        db.collection('tasks').insertMany([{
-            description: 'Take out trash',
-            complete: false
-        }, 
-        {
-            description: 'Feed the dog',
-            complete: true
-        },
-        {
-            description: 'Kick the cat',
-            complete: false
-        }], (error, result) => {
+        // db.collection('tasks').insertMany([{
+        //     description: 'Take out trash',
+        //     complete: false
+        // }, 
+        // {
+        //     description: 'Feed the dog',
+        //     complete: true
+        // },
+        // {
+        //     description: 'Kick the cat',
+        //     complete: false
+        // }], (error, result) => {
 
-            if (error) {
-                return console.log('Unable to insert task')
-            }
-            console.log(result.result)
-            console.log(result.ops)
-        })
+        //     if (error) {
+        //         return console.log('Unable to insert task')
+        //     }
+        //     console.log(result.result)
+        //     console.log(result.ops)
+        // })
+
+
 })
 
