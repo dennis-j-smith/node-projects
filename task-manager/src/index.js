7168269e2ad826a07f611ae0cb208d5231af793d
Dennis Smith <dennis.j.smith95@gmail.com>
2020-11-18 20:06:28 -0500

    middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,6 +5,12 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+
+//     res.status(503).send("Site under Maintenance");
+//     //next()
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
