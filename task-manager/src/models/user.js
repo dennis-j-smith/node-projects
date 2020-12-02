b4df782ab22636601612e98ba299e6a8222a8c08
Dennis Smith <dennis.j.smith95@gmail.com>
2020-12-01 20:16:52 -0500

    updates

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -48,6 +49,12 @@ const userSchema = new mongoose.Schema({
     }]
 });
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
 userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({ _id: user._id.toString()}, 'thisismynewcourse')
@@ -88,6 +95,14 @@ userSchema.pre('save', async function(next) {
         user.password = await bcrypt.hash(user.password, 8)
     }    
     next()
+})
+
+// Delete tasks when user is removed
+userSchema.pre('remove', async function(next) {
+
+    const user = this
+    await Task.deleteMany({owner: user._id})
+    next();
 })
 
 const User = mongoose.model('User', userSchema)
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -63,29 +63,8 @@ router.get('/users/me', auth, async (req, res) => {
      res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
 
-    // User.findById(req.params.id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-    //     res.send(user)
-    // }).catch((e) => {
-    //     res.status(500).send(e)
-    // });   
-
-    try {
-        const user = await User.findById(req.params.id)
-        if (!user) {
-            res.status(404).send()    
-        }
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);    
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => {
@@ -98,28 +77,19 @@ router.patch('/users/:id', async (req, res) => {
 
     try {
 
-        const user = await User.findById(req.params.id)
 
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save();
-
-        //const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
-        if (!user) {
-            res.status(404).send()    
-        }
-        res.send(user)
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save();
+        res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.delete('/user/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) {
-            res.status(404).send()    
-        }
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send(e)
     }
