b8239aa3ea77f8d71ebf7f75297afa970b57f83e
Dennis Smith <dennis.j.smith95@gmail.com>
2020-12-22 13:34:50 -0500

    latest:

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const sharp = require('sharp')
 const router = new express.Router()
+const multer = require('multer')
+
+const upload = multer ({    
+    limits: {
+        fileSize: 1000000,        
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('File must be a JPG, JPEG, or PNG'))
+        }   
+        cb(undefined, true)
+    }
+})
+
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body) 
@@ -76,8 +91,6 @@ router.patch('/users/me', auth, async (req, res) => {
     }
 
     try {
-
-
         updates.forEach((update) => req.user[update] = req.body[update])
         await req.user.save();
         res.send(req.user)
@@ -94,5 +107,39 @@ router.delete('/users/me', auth, async (req, res) => {
         res.status(500).send(e)
     }
 })
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
+    
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message});
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
+}
+
+)
 
 module.exports = router
