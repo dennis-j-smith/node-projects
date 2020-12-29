6cd902512c7a840aa4fbb5536adc1c11b0d71dd4
Dennis Smith <dennis.j.smith95@gmail.com>
2020-12-29 10:46:08 -0500

    updates

---
+++ task-manager/tests/math.test.js
@@ -0,0 +1,10 @@
+const {
+    fahrenheitToCelsius,
+    celsiusToFahrenheit
+    } = require("../src/math");
+    
+test("Should convert 32 F to 0 C", () =>
+expect(fahrenheitToCelsius(32)).toBe(0));
+    
+test("Should convert 0 C to 32 F", () =>
+expect(celsiusToFahrenheit(0)).toBe(32));
