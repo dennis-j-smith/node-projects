bf1b23da0cbbfdae6ee42e0df6a08b947960e5d8
Dennis Smith <dennis.j.smith95@gmail.com>
2021-01-26 20:28:37 -0500

    updates

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,19 @@
-io()
+const socket = io()
+
+socket.on('welcome', (message) => {
+    console.log(message)
+})
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+const messageForm = document.querySelector('form')
+
+
+messageForm.addEventListener('submit', (e) => { 
+    e.preventDefault()   
+    const message = e.target.elements.message
+    const msg = message.value  
+    socket.emit('sendMessage', msg)
+})
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -6,8 +6,15 @@ const http = require('http')
 const server = http.createServer(app)
 const io = socketio(server);
 
-io.on('connection', () => {
+
+io.on('connection', (socket) => {
     console.log('New WebSocket Connection')
+
+    socket.emit('welcome', 'Welcome to my cool new chat!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
+    })
 })
 server.listen(port, () => {
     console.log('Server is up on port ' + port)
